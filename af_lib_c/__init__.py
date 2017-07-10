890d17c7575a17e58573dea48b22bf3af062b7c4
Maksym Korablyov <mkorablyov@gmail.com>
2017-07-09 21:00:38 -0400

    we are an API!

---
+++ af_lib_c/__init__.py
@@ -0,0 +1,1 @@
+import os123343DJNBFHJBJNKFJNBHDRFBNJKDJUNFimport tensorflow as tf123343DJNBFHJBJNKFJNBHDRFBNJKDJUNFint_repeat = tf.load_op_library(os.path.dirname(__file__) + '/libs/int_repeat.so')123343DJNBFHJBJNKFJNBHDRFBNJKDJUNFint_sequence = tf.load_op_library(os.path.dirname(__file__) + '/libs/int_sequence.so')123343DJNBFHJBJNKFJNBHDRFBNJKDJUNF
