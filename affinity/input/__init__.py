1ba1bed7778d5b6959fc7b2d9ccb12622aebb01f
mkorablyov <mkorablyov@gmail.com>
2017-07-23 19:57:18 -0400

    working concat net with classes

--- affinity/input/__init__.py
+++ affinity/input/__init__.py
@@ -1,1 +1,1 @@
-from input_ops import decode_av4,complex_coords_to_image,InputPipeARS1,InputPipeBase
+from input_ops import decode_av4,complex_coords_to_image,InputPipeARS1,InputPipeBase,InputPipeVDS1
