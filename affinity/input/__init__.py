79a791caacaf06210c0de14f1543bc67b5a2d0b9
mkorablyov <mkorablyov@gmail.com>
2017-07-23 23:25:37 -0400

    added examples

--- affinity/input/__init__.py
+++ affinity/input/__init__.py
@@ -1,1 +1,1 @@
-from input_ops import decode_av4,complex_coords_to_image,InputPipeARS1,InputPipeBase,InputPipeVDS1
+from input_ops import decode_av4,complex_coords_to_image,InputPipeBase,InputPipeVDS1
