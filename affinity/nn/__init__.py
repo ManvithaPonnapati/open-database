c6d636bbcc536026a1b206576937c85a7ec5ed92
mkorablyov <mkorablyov@gmail.com>
2017-07-29 00:37:02 -0400

    not working protos

--- affinity/nn/__init__.py
+++ affinity/nn/__init__.py
@@ -1,1 +1,1 @@
-from nn_ops import concat_nonlinear_conv3d, atomic_convolution_layer, atomistic_fc_layer, atomistic_output_layer123343DJNBFHJBJNKFJNBHDRFBNJKDJUNF
+from nn_ops import concat_nonlinear_conv3d,concat_nonlinear_conv3d_v2,atomic_convolution_layer, atomistic_fc_layer, \123343DJNBFHJBJNKFJNBHDRFBNJKDJUNF    atomistic_output_layer123343DJNBFHJBJNKFJNBHDRFBNJKDJUNF
