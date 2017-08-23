a01a37392e621a6594e3802114d731398ab527a9
LXander <l.xander.233@gmail.com>
2017-08-23 22:13:58 +0800

    db_libs update

---
+++ affinityDB/db_libs/reorder_op.py
@@ -0,0 +1,157 @@
+import os,sys
+import subprocess
+import json 
+
+class Reorder_init:
+    this_module = sys.modules[__name__]
+    def __init__(self, data_dir, reorder_folder, smina_path):
+        reorder_dir = os.path.join(data_dir, reorder_folder)
+        if not os.path.exists(reorder_dir):
+            os.makedirs(reorder_dir)
+
+        self.data_dir = data_dir
+        self.reorder_folder = reorder_folder
+        self.smina_path = smina_path
+        self.this_module.reorder_init = self
+
+def reorder(rec_outpath, lig_outpath, init='reorder_init'):
+
+    init = eval(init)
+    reorder_dir = os.path.join(init.data_dir, init.reorder_folder) 
+    rec_path = os.path.join(init.data_dir, rec_outpath)
+    lig_path = os.path.join(init.data_dir, lig_outpath)
+
+    reo_name = os.path.basename(rec_path).replace('receptor','reorder')
+    receptor = os.path.basename(rec_outpath).split('_')[0]
+    out_path = os.path.join(reorder_dir, receptor, reo_name)
+
+    smina_pm = smina_param(init.smina_path)
+    smina_pm.param_load({
+        'args':['score_only']})
+    if not os.path.exists(os.path.dirname(out_path)):
+        os.makedirs(os.path.dirname(out_path))
+
+    kw = {
+        'receptor': rec_path,
+        'ligand': lig_path,
+        'autobox_ligand':lig_path,
+        'out':out_path
+    }       
+
+    cmd = smina_pm.make_command(**kw)
+    cl = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    cl.wait()
+
+    return [[rec_outpath, os.path.join(init.reorder_folder, receptor, reo_name)]]
+
+class smina_param:
+
+    kw_options = [
+        'receptor',
+        'ligand',
+        'out',
+        'flex',
+        'flexres',
+        'flexdist_ligand',
+        'flexdist',
+        'center_x',
+        'center_y',
+        'center_z',
+        'size_x',
+        'size_y',
+        'size_z',
+        'autobox_ligand',
+        'autobox_add',
+        'scoring',
+        'custom_scoring',
+        'minimize_iters',
+        'approximation',
+        'factor',
+        'force_cap',
+        'user_grid',
+        'user_grid_lambda',
+        'out_flex',
+        'log',
+        'atom_terms',
+        'cpu',
+        'seed',
+        'exhaustiveness',
+        'num_modes',
+        'energy_range',
+        'min_rmsd_filter',
+        'addH',
+        'config'
+    ]
+
+    arg_options = [
+        'no_lig',
+        'score_only',
+        'local_only',
+        'minimize',
+        'randomize_only',
+        'accurate_line',
+        'print_terms',
+        'print_atom_types',
+        'atom_term_data',
+        'quiet',
+        'flex_hydrogen',
+        'help',
+        'help_hidden',
+        'version'
+    ]
+
+    def __init__(self, smina_path, name=None):
+        self.smina = smina_path
+
+        if not name is None:
+            self.name = name
+        
+    def set_smina(self, smina):
+        self.smina = smina
+
+    def set_name(self, name):
+        self.name = name
+
+    def load_param(self, *arg, **kw):
+        self.args = arg
+        self.kwargs = kw
+
+    def param_dump(self):
+        param = {
+            'args':self.args,
+            'kwargs':self.kwargs
+        }
+        
+        return param
+        
+    def param_load(self, param):
+        if type(param).__name__ in ['unicode','str']:
+            param = json.loads(param)
+        
+        self.args = param['args']
+        self.kwargs = param['kwargs']
+        
+
+    def make_command(self, *arg, **kw):
+        cmd = self.smina
+        for a in self.arg_options:
+            if arg and a in arg:
+                cmd += ' --'
+                cmd += a
+            elif a in self.args:
+                cmd += ' --'
+                cmd += a
+
+        for key in self.kw_options:
+            if kw and key in kw.keys():
+                cmd += ' --'
+                cmd += key
+                cmd += ' '
+                cmd += str(kw[key])
+            elif key in self.kwargs.keys():
+                cmd += ' --'
+                cmd += key
+                cmd += ' '
+                cmd += str(self.kwargs[key])
+
+        return cmd
---
+++ affinityDatasets/VDS1/make_example.py
@@ -0,0 +1,80 @@
+import os,sys,time 
+import numpy as np 
+sys.path.append('../../affinityDB')
+sys.path.append('../../affinityDB/db_libs')
+import database, sqlite3
+import imp
+
+db_dir = os.path.join(os.path.abspath(os.getcwd()), 'database')
+db_path = os.path.join(db_dir, 'database.db')
+data_dir = os.path.join(db_dir, 'data')
+
+
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+
+afdb = database.AffinityDB(db_path)
+database_master = database.DatabaseMaster(db_path)
+
+
+with open('../VDS1/data/main_pdb_target_list.txt') as f: pdb_list = f.readline().strip().split(', ')
+pdb_list = pdb_list[:2]
+pdb_list = [str(p) for p in pdb_list]
+
+
+fp, path, descr = imp.find_module('download_pdb_op')
+lib_mod = imp.load_module('download_pdb_op', fp, path, descr)
+lib_mod.Download_pdb_init(db_path=data_dir, download_dir = 'download')
+
+
+afdb.run_multithread("download_pdb",
+                    arg_types=[str],
+                    arg_lists=[pdb_list],
+                    out_types=[str],
+                    out_names=["pdb_path"])
+
+inputs = database_master.retrieve('out_000_download_pdb',['pdb_path'],{"run_idx":"{}>=0"})
+print (inputs)
+
+pdb_paths = list(map(str,inputs[0]))
+
+fp, path, descr = imp.find_module('split_pdb_op')
+lib_mod = imp.load_module('split_pdb_op', fp, path, descr)
+lib_mod.Split_pdb_init(db_path=data_dir, split_dir='split')
+
+afdb.run_multithread(func='split_pdb',
+                     arg_types=[str],
+                     arg_lists = [pdb_paths],
+                     out_types=[str,str],
+                     out_names = ['lig_outpath','rec_outpath'])
+
+
+inputs = database_master.retrieve('out_001_split_pdb',['rec_outpath','lig_outpath'],{"run_idx":"{}>=0"})
+rec_outpaths = list(map(str,inputs[0]))
+lig_outpaths = list(map(str,inputs[1]))
+
+smina_path = '/Users/Will/projects/smina/smina.osx'
+fp, path, descr = imp.find_module('reorder_op')
+lib_mod = imp.load_module('reorder_op', fp, path, descr)
+lib_mod.Reorder_init(data_dir=data_dir, reorder_folder = 'reorder', smina_path=smina_path)
+
+afdb.run_multithread(func='reorder',
+                     arg_types=[str,str],
+                     arg_lists=[rec_outpaths, lig_outpaths],
+                     out_types = [str,str],
+                     out_names = ['rec_outpath','reorder_outpath'])
+
+inputs = db_editor.retrieve('out_002_reorder',['rec_outpath','reorder_outpath'],{"run_idx":"{}>=0"})
+
+
+rec_outpaths = list(map(str,inputs[0]))
+reorder_outpaths = list(map(str,inputs[1]))
+fp, path, descr = imp.find_module('dock_op')
+lib_mod = imp.load_module('dock_op', fp, path, descr)
+lib_mod.Dock_init(data_dir=data_dir, dock_folder = 'dock', smina_path=smina_path)
+
+afdb.run_multithread(func='dock',
+                     arg_types=[str,str],
+                     arg_lists = [rec_outpaths, reorder_outpaths],
+                     out_types = [str,str,str],
+                     out_names=['rec_outpath','reorder_outpath','dock_outpath'])
