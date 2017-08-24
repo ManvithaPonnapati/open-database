2243e848ca117b64ca8077643c4bcfaa01e9a986
LXander <l.xander.233@gmail.com>
2017-08-24 15:04:27 +0800

    format ops

--- affinityDB/db_libs/split_pdb_op.py
+++ affinityDB/db_libs/split_pdb_op.py
@@ -1,7 +1,7 @@
 import os,sys
 import prody as pr
 
-# FIXME: needs a cutoff distance to be implemented
+
 
 class Split_pdb_init:
     this_module = sys.modules[__name__]
@@ -18,13 +18,15 @@ class Split_pdb_init:
         self.this_module.split_pdb_init = self
 
 
-def split_pdb(split_pdb, cuttoff_dist = None, discard_h = True,init="split_pdb_init"):
+def split_pdb(split_pdb, cuttoff_dist = 8, min_rec_atom_num=100, min_lig_atom_num = 5, discard_h = True,init="split_pdb_init"):
     """ Iterates through every molecule of the ligand in the receptor. Crops atoms of the receptor (and any chemicals,
      but not water) within within the cutoff  distance of any atoms of the ligand. Saves ligand + binding site crop
      as pairs into folders.
 
     :param split_pdb: string (relative path the the file to split)
     :param cuttoff_dist: float (distance of any atoms in the binding site from any atom of the ligand to be saved)
+    :param min_rec_atom_num: minimun number of atoms be saved as binidng site
+    :param min_lig_atom_num: minumum number of atoms for ligand to be saved
     :param discard_h: Bool T/F (discard all hydrogens ?)
     :param init: string (init function in this module)
     :return:
@@ -50,6 +52,34 @@ def split_pdb(split_pdb, cuttoff_dist = None, discard_h = True,init="split_pdb_i
             lig = pr_pdb.select('chain {} resnum {}'.format(chain, resnum))
             rec = pr_pdb.select('not water not (chain {} resnum {})'.format(chain, resnum))
 
+        lig_coords = lig.getCoords()
+        lig_atom_num = lig.numAtoms()
+        if lig_atom_num < min_lig_atom_num:
+            raise Exception("Ligand atom num {} smaller than {} ".format(lig_atom_num, min_lig_atom_num))
+
+        rec_atom_num = lig.numAtoms()
+        if rec_atom_num < min_rec_atom_num:
+            raise Exception("Receptor atom num {} smaller than {}".format(rec_atom_num, min_rec_atom_num))
+
+
+        # select atom from receptor around ligand
+        while True:
+            res_coll = []
+            for center in lig_coords:
+                around_atoms = rec.select('within {} of center'.format(cuttoff_dist), center=center)
+                if around_atoms is None:
+                    continue
+                res_coll.append(around_atoms)
+
+            binding_site = reduce(lambda x,y : x|y, res_coll)
+            binding_atom_num = binding_site.numAtoms()
+            if binding_atom_num < min_rec_atom_num:
+                cuttoff_dist +=1
+            else:
+                break
+
+        
+
         # write the ligand file and the receptor file
         pair_name = '_'.join([pdb_id, chain, resnum, resname])
         lig_name = pair_name + "_ligand.pdb"
@@ -58,6 +88,6 @@ def split_pdb(split_pdb, cuttoff_dist = None, discard_h = True,init="split_pdb_i
         lig_outpath = os.path.join(init.split_dir, pair_name, lig_name)
         rec_outpath = os.path.join(init.split_dir, pair_name, rec_name)
         pr.writePDB(os.path.join(init.db_root,lig_outpath), lig)
-        pr.writePDB(os.path.join(init.db_root,rec_outpath), rec)
+        pr.writePDB(os.path.join(init.db_root,rec_outpath), binding_site)
         out_filenames.append([lig_outpath,rec_outpath])
     return out_filenames
--- affinityDatasets/BS1/make_example.py
+++ affinityDatasets/BS1/make_example.py
@@ -85,5 +85,5 @@ lib_mod.Activity_init(data_dir=data_dir, activity_folder = 'activity')
 afdb.run_multithread("activity",
                     arg_types=[str, str],
                     arg_lists=[pair_names, target_idx],
-                    out_types=[str, str, str, str, str, str, str, float, str],
-                    out_names=['pair_name','target_id','aid','mid','smile','measure','op','value','unit'])
+                    out_types=[str, str, str, str, str, str, str, float, str, int],
+                    out_names=['pair_name','target_id','aid','mid','smile','measure','op','value','unit', 'confidence'])
